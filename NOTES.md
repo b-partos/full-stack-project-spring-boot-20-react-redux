bf0ae125429fd6114d2ea782d9b2c3994eb12e08
partosb <bal.partos@gmail.com>
2020-12-25 12:05:46 +0100

    State before restarting the tutorial.

---
+++ NOTES.md
@@ -0,0 +1,8 @@
+# Notes
+
+## Installed packages via npm
+```
+npm install create-react-app
+./node_modules/.bin/create-react-app ppmtool-react-client --verbose | tee npm.outú
+npm install bootstrap
+```
---
+++ src/components/Project/ProjectItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Dashboard from "../Dashboard";
+
+class ProjectItem extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Dashboard />
+            </div>
+        );
+    }
+}
+
+export default ProjectItem;
